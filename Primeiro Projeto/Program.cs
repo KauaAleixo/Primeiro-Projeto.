23ebd001c5f6c19824c8b6da2a1e53826dc5b272
cj3021947 <cj3021947@LAB01-PC34>
2024-02-21 10:30:31 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace Primeiro_Projeto_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                string nome = "Kauã Aleixo";
+                int idade = 16;
+                string cidade = "Campos Do Jordão";
+
+                Console.WriteLine("digite seu nome");
+
+                nome = Console.ReadLine();
+
+
+                Console.WriteLine("digite sua idade");
+               
+                idade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite sua cidade");
+
+                cidade = Console.ReadLine();
+
+                {
+                    ;
+                }
+            }
+        }
+    }
+
+
+}
